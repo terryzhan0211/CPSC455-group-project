e5a73893956f56b49d6051764098968afd9bd1f3
Xinye Zhan <zxy20001@student.ubc.ca>
2022-06-11 00:09:03 -0700

    postDetail page

--- front-end/src/components/Post.js
+++ front-end/src/components/Post.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import './Post.css';
+import { Link } from 'react-router-dom';
+import { BiZoomIn } from "react-icons/bi";
+
 
 function Post(props) {
 	return (
@@ -7,7 +10,11 @@ function Post(props) {
 			<img src={props.path} alt="post"></img>
 			<div className="post-content">
 				<p>
-					<strong>@{props.userName}</strong> {props.title}
+					<strong>@{props.userName}</strong> {props.title} 
+					<Link to="/postdetail"  className="back-button"> 
+						<BiZoomIn className='btn-zoom-in' style={{color: 'white'}} />
+					</Link>
+					
 				</p>
 			</div>
 		</div>
---
+++ front-end/src/components/PostData.js
@@ -0,0 +1,11 @@
+import God from "../img/god.jpg";
+import Test from "../img/test1.jpg";
+
+export const PostData = [
+    {   id:1, 
+        image: God
+    },
+    {   id:2, 
+        image: Test
+    },
+]
