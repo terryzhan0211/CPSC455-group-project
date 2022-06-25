fcf776dd9a29ac8b9f3a2b1fe803d863d32b25b1
knzheng <knzheng@student.ubc.ca>
2022-06-24 22:28:26 -0700

    fix posts render

--- front-end/src/components/Posts.js
+++ front-end/src/components/Posts.js
@@ -11,27 +11,37 @@ import { getPosts } from '../features/cities';
 function Posts(props) {
 	const img = require('../img/test1.jpg');
 	const dispatch = useDispatch();
-	const posts = useSelector((state) => state.currPosts);
+	const posts = useSelector((state) => state.cities.currPosts);
 	// const posts = [];
 	// const { state } = useLocation();
 	// const { cityName } = state;
-	const renderPosts = () => {
-		posts.map((post, index) => {
-			return (
-				<div className="posts-item" key={index}>
-					<Post
-						path={img}
-						userName={post.username}
-						title={post.title}
-						content={post.content}
-					/>
-				</div>
-			);
-		});
-	};
+
+	// useEffect(() => {
+	// 	// dispatch(getPosts(props.cityName));
+	//
+	// });
+
+	const [renderPosts, setRenderPosts] = useState();
+	const [showPosts, setShowPosts] = useState(false);
 	useEffect(() => {
 		dispatch(getPosts(props.cityName));
-	});
+		console.log(posts);
+		setRenderPosts(() => {
+			return posts.posts.map((post, index) => {
+				return (
+					<div className="posts-item" key={index}>
+						<Post
+							path={img}
+							userName={post.username}
+							title={post.title}
+							content={post.content}
+						/>
+					</div>
+				);
+			});
+		});
+		// setShowPosts(true);
+	}, []);
 	return (
 		<div>
 			<div>
