81cd8f48a10c3df39e8d83ba45a758fb385f78d8
Shawn Gu <shawn.gu.92@gmail.com>
2022-07-15 14:30:42 -0700

    update

--- front-end/src/features/user.js
+++ front-end/src/features/user.js
@@ -1,7 +1,7 @@
-import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
-import userService from './userService'
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import userService from './userService';
 
-const user = JSON.parse(localStorage.getItem('user'))
+const user = JSON.parse(localStorage.getItem('user'));
 const noUserState = {
 	id: '-1',
 	username: 'visitor',
@@ -11,35 +11,27 @@ const noUserState = {
 	likedPosts: ['97345116-0409-4b1e-b195-f9c2aa684f90'],
 };
 
-
 const initialState = {
-  user: user ? user : null,
-  isLogin: user ? true : false,
-  isError: false,
-  isSuccess: false,
-  isLoading: false,
-  message: '',
-
-}
-
+	user: user ? user : null,
+	isLogin: user ? true : false,
+	isError: false,
+	isSuccess: false,
+	isLoading: false,
+	message: '',
+};
 
 // Register
-export const register = createAsyncThunk(
-  'user/signup',
-  async (user, thunkAPI) => {
-    try {
-      return await userService.register(user)
-    } catch (error) {
-      const message =
-        (error.response &&
-          error.response.data &&
-          error.response.data.message) ||
-        error.message ||
-        error.toString()
-      return thunkAPI.rejectWithValue(message)
-    }
-  }
-)
+export const register = createAsyncThunk('user/signup', async (user, thunkAPI) => {
+	try {
+		return await userService.register(user);
+	} catch (error) {
+		const message =
+			(error.response && error.response.data && error.response.data.message) ||
+			error.message ||
+			error.toString();
+		return thunkAPI.rejectWithValue(message);
+	}
+});
 
 // export const likePostAsync = createAsyncThunk(
 //   'user/signup',
@@ -60,110 +52,109 @@ export const register = createAsyncThunk(
 
 // Login
 export const login = createAsyncThunk('user/login', async (user, thunkAPI) => {
-  try {
-    return await userService.login(user)
-  } catch (error) {
-    const message =
-      (error.response && error.response.data && error.response.data.message) ||
-      error.message ||
-      error.toString()
-    return thunkAPI.rejectWithValue(message)
-  }
-})
+	try {
+		return await userService.login(user);
+	} catch (error) {
+		const message =
+			(error.response && error.response.data && error.response.data.message) ||
+			error.message ||
+			error.toString();
+		return thunkAPI.rejectWithValue(message);
+	}
+});
 
 // Edit user info
 export const editUser = createAsyncThunk('user/edit', async (user, thunkAPI) => {
-  try {
-    const token = thunkAPI.getState().user.user.token
-    return await userService.editUser(user,token)
-  } catch (error) {
-    const message =
-      (error.response && error.response.data && error.response.data.message) ||
-      error.message ||
-      error.toString()
-    return thunkAPI.rejectWithValue(message)
-  }
-})
+	try {
+		const token = thunkAPI.getState().user.user.token;
+		return await userService.editUser(user, token);
+	} catch (error) {
+		const message =
+			(error.response && error.response.data && error.response.data.message) ||
+			error.message ||
+			error.toString();
+		return thunkAPI.rejectWithValue(message);
+	}
+});
 
 export const logout = createAsyncThunk('user/logout', async () => {
-  await userService.logout()
-})
+	await userService.logout();
+});
 
 export const userSlice = createSlice({
-  name: 'user',
-  initialState,
-  reducers: {
-    reset: (state) => {
-      state.isLoading = false
-      state.isSuccess = false
-      state.isError = false
-      state.message = ''
-    },
-    unlikePost:(state,action) =>{
-			let newLikedPosts = state.user.likedPosts.filter((postID)=>{
-				return postID !== action.payload
+	name: 'user',
+	initialState,
+	reducers: {
+		reset: (state) => {
+			state.isLoading = false;
+			state.isSuccess = false;
+			state.isError = false;
+			state.message = '';
+		},
+		unlikePost: (state, action) => {
+			let newLikedPosts = state.user.likedPosts.filter((postID) => {
+				return postID !== action.payload;
 			});
 			state.user.likedPosts = newLikedPosts;
 		},
-		likePost:(state,action) =>{		
-      console.log(state.user.likedPosts)
-			state.user.likedPosts.push(action.payload.postID);
+		likePost: (state, action) => {
+			state.user.likedPosts.push(action.payload);
 		},
-  },
-  extraReducers: (builder) => {
-    builder
-      .addCase(register.pending, (state) => {
-        state.isLoading = true
-      })
-      .addCase(register.fulfilled, (state, action) => {
-        state.isLoading = false
-        state.isSuccess = true
-        state.user = action.payload
-        state.isLogin = true
-      })
-      .addCase(register.rejected, (state, action) => {
-        state.isLoading = false
-        state.isError = true
-        state.message = action.payload
-        state.user = null
-      })
-      .addCase(login.pending, (state) => {
-        state.isLoading = true
-      })
-      .addCase(login.fulfilled, (state, action) => {
-        state.isLoading = false
-        state.isSuccess = true
-        state.user = action.payload
-        state.isLogin = true
-      })
-      .addCase(login.rejected, (state, action) => {
-        state.isLoading = false
-        state.isError = true
-        state.message = action.payload
-        state.user = null
-      })
-      .addCase(logout.fulfilled, (state) => {
-        state.user = null
-        state.isLogin = false
-      })
-      .addCase(editUser.pending, (state) => {
-        state.isLoading = true
-      })
-      .addCase(editUser.fulfilled, (state, action) => {
-        state.isLoading = false
-        state.isSuccess = true
-        state.user = action.payload
-      })
-      .addCase(editUser.rejected, (state, action) => {
-        state.isLoading = false
-        state.isError = true
-        state.message = action.payload
-      })
-  },
-})
+	},
+	extraReducers: (builder) => {
+		builder
+			.addCase(register.pending, (state) => {
+				state.isLoading = true;
+			})
+			.addCase(register.fulfilled, (state, action) => {
+				state.isLoading = false;
+				state.isSuccess = true;
+				state.user = action.payload;
+				state.isLogin = true;
+			})
+			.addCase(register.rejected, (state, action) => {
+				state.isLoading = false;
+				state.isError = true;
+				state.message = action.payload;
+				state.user = null;
+			})
+			.addCase(login.pending, (state) => {
+				state.isLoading = true;
+			})
+			.addCase(login.fulfilled, (state, action) => {
+				state.isLoading = false;
+				state.isSuccess = true;
+				state.user = action.payload;
+				state.isLogin = true;
+			})
+			.addCase(login.rejected, (state, action) => {
+				state.isLoading = false;
+				state.isError = true;
+				state.message = action.payload;
+				state.user = null;
+			})
+			.addCase(logout.fulfilled, (state) => {
+				state.user = null;
+				state.isLogin = false;
+			})
+			.addCase(editUser.pending, (state) => {
+				state.isLoading = true;
+			})
+			.addCase(editUser.fulfilled, (state, action) => {
+				state.isLoading = false;
+				state.isSuccess = true;
+				state.user = action.payload;
+			})
+			.addCase(editUser.rejected, (state, action) => {
+				state.isLoading = false;
+				state.isError = true;
+				state.message = action.payload;
+			});
+	},
+});
 
-export const { reset,unlikePost, likePost} = userSlice.actions
-export default userSlice.reducer
+export const { reset, unlikePost, likePost } = userSlice.actions;
+export default userSlice.reducer;
 
 // import { createSlice } from '@reduxjs/toolkit';
 // import { createAsyncThunk } from '@reduxjs/toolkit';
